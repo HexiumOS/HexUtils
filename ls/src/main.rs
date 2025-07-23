30502e6606d34c0a3e91379fbc6cbb7a33b8092c
The Douck <thedouckdev@gmail.com>
2025-07-23 22:36:01 +0200

    Started with ls.

---
+++ ls/src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use std::fs::{self, DirEntry};
+use std::path::Path;
+use std::env;
+use ansi_term::Colour;
+use is_executable::IsExecutable;
+
+// one possible implementation of walking a directory only visiting files
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            cb(&entry);
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            }
+        }
+    }
+    Ok(())
+}
+
+
+fn main() -> std::io::Result<()> {
+    let dir = env::current_dir()?;
+    visit_dirs(&dir, &|entry| {
+        let path = entry.path();
+        if path.is_dir() {
+            println!("{}", Colour::RGB(0, 0, 255).paint(entry.file_name().to_string_lossy()));
+        } else if path.is_executable() {
+            println!("{}", Colour::RGB(0, 255, 0).paint(entry.file_name().to_string_lossy()));
+        } else {
+            println!("{}", Colour::RGB(255, 255, 255).paint(entry.file_name().to_string_lossy()));
+        }
+    })?;
+    Ok(())
+}
